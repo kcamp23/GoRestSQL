05bb90755b4f181377e017f891d4e9ca8a45a8f7
Kat Camp <kcamp552369@gmail.com>
2022-04-08 14:18:19 -0400

    Initial commit

---
+++ src/main/java/com/careerdevs/GoRestSQL/GoRestSqlApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.GoRestSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoRestSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoRestSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/careerdevs/GoRestSQL/GoRestSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.GoRestSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoRestSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
